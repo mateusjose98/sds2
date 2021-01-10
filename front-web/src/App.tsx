673546b4f2fba855c6947fe0fc5d08faff133c6e
mateusjose98 <josemateus.ufma@gmail.com>
2021-01-09 21:43:52 -0300

    Add home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,14 +1,16 @@
 import './App.css';
 import NavBar from './Navbar';
+import Home from './Home';
 
 
 function App() {
   return (
-    <div>
-
+    
+      <>
       <NavBar/>
-
-    </div>
+      <Home />
+    </>
+   
   );
 }
 
---
+++ front-web/src/Home/index.tsx
@@ -0,0 +1,32 @@
+import './styles.css';
+
+import { ReactComponent as MainImage} from './main.svg';
+
+function Home() {
+    return (
+
+        <div className="home-container">
+
+            <div className="home-content">
+                <div className="home-actions">
+                    <h1 className="home-title">
+                        Faça seu pedido <br/> que entregamos <br/> para você!
+                     </h1>
+
+                     <h2 className="home-subtitle">
+                         Escolha o seu pedido e em poucos minutos <br/> levaremos na sua porta
+                     </h2>
+                     <a className="home-btn-order" href="#">FAZER PEDIDO</a>
+
+                </div>
+
+                <div className="home-image">
+                <MainImage />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Home;
