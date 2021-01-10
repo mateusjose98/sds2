8d977267adc783bf066cab9bb874e0f30dc712ff
mateusjose98 <josemateus.ufma@gmail.com>
2021-01-10 10:26:25 -0300

    teste-readme

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,12 +1,29 @@
 import './styles.css';
 import { ReactComponent as Logo } from './logo.svg';
 import StepsHeader from './StepsHeader';
+import React, { useEffect, useState } from 'react';
+import ProductsList from './ProductsList';
+import { Product } from './types';
+import { fetchProducts } from '../api';
 
 function Orders() {
+    const [products, setProducts] = useState<Product[]>([]);
+
+    console.log(products)
+    useEffect(() =>{
+       fetchProducts()
+       .then(response => setProducts(response.data))
+       .catch(error => console.error(error))
+    },[])
+
+
+
+
     return (
         
         <div className="orders-container">
             <StepsHeader />
+            <ProductsList products={products}/>
         </div>
     )
 }
